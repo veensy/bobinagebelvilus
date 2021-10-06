6f05f751a8576d88f6c8a928ef6e6a59a7de7b62
keveens-belvilus <keveens.belvilus@manomano.com>
2021-10-06 14:40:58 +0200

    change home images

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Card } from '../../components/Card/Card';
 import about from '../../assets/pancarte.png';
+import moteur from '../../assets/catalogue/img_1.jpeg';
+import bobinage from '../../assets/catalogue/img_5.jpeg';
 
 import styles from './Home.module.scss';
 import { SOCIÉTÉ, CATALOGUE, EXPERTISE } from '../../components/Nav/constants';
 import { COMPANY_TEXT } from '../Company/constants';
-import { EXPERTISE_TEXT } from '../Expertise/constants'
-import { CATALOGUE_TEXT } from '../Catalogue/constants'
+import { EXPERTISE_TEXT } from '../Expertise/constants';
+import { CATALOGUE_TEXT } from '../Catalogue/constants';
 
 interface HomeInt {
   onClick?: (arg0: string) => void;
@@ -16,11 +18,19 @@ export const Home = ({ onClick }: HomeInt) => {
     url: about,
     alt: 'pancarte',
   };
+  const catalogueImg = {
+    url: moteur,
+    alt: 'moteur',
+  };
+  const bobinageImg = {
+    url: bobinage,
+    alt: 'bobinage',
+  };
   const readMore = `EN SAVOIR PLUS  >`;
   const cards = [
     { header: SOCIÉTÉ, text: COMPANY_TEXT, image, readMore },
-    { header: CATALOGUE, text: CATALOGUE_TEXT, image, readMore },
-    { header: EXPERTISE, text: EXPERTISE_TEXT, image, readMore },
+    { header: CATALOGUE, text: CATALOGUE_TEXT, image: catalogueImg, readMore },
+    { header: EXPERTISE, text: EXPERTISE_TEXT, image: bobinageImg, readMore },
   ];
 
   return (
